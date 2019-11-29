c3edb777359604910226117a65dda3217c2a02df
Bastien <bas.dijkstra@student.hku.nl>
2019-11-29 16:19:40 +0100

    Robot IK working for gameplay

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,23 +7,23 @@ public class PlayerController : MonoBehaviour
     public GameObject Hand;
 
     [Header("Hand variables")]
-    [SerializeField] private float handDistanceNormal;
-    [SerializeField] private float handDistanceStretched;
-    [SerializeField] private float stretchSpeed;
-    [SerializeField] private float handReach;
-    [SerializeField] private float handHeight;
+    [SerializeField] private float handDistanceNormal = 0;
+    [SerializeField] private float handDistanceStretched = 0;
+    [SerializeField] private float stretchSpeed = 0;
+    [SerializeField] private float handReach = 0;
+    [SerializeField] private float handHeight = 0;
 
     [Header("Body variables")]
-    [SerializeField] private float rotateSpeed;
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotateSpeed = 0;
+    [SerializeField] private float moveSpeed = 0;
 
     [Header("Rail variables")]
-    [SerializeField] private Transform railA;
-    [SerializeField] private Transform railB;
+    [SerializeField] private Transform railA = default;
+    [SerializeField] private Transform railB = default;
 
     [Header("Pig variables")]
-    [SerializeField] private GameObject pigPrefab;
-    [SerializeField] private Transform pigSpawnPosition;
+    [SerializeField] private GameObject pigPrefab = default;
+    [SerializeField] private Transform pigSpawnPosition = default;
 
     private float handDistance;
     private bool stretching;
@@ -36,7 +36,7 @@ public class PlayerController : MonoBehaviour
     private float buttonWaitTime = 3f;
     private bool buttonPressed = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -44,10 +44,13 @@ public class PlayerController : MonoBehaviour
 
         Cursor.visible = false;
         handDistance = handDistanceNormal;
-        if (Hand.GetComponent<CollisionObject>().triggerEvent == null)
-            Hand.GetComponent<CollisionObject>().triggerEvent = new TriggerEvent();
+        if (Hand.GetComponent<CollisionObject>() != null)
+        {
+            if (Hand.GetComponent<CollisionObject>().triggerEvent == null)
+                Hand.GetComponent<CollisionObject>().triggerEvent = new TriggerEvent();
 
-        Hand.GetComponent<CollisionObject>().triggerEvent.AddListener(HandTrigger);
+            Hand.GetComponent<CollisionObject>().triggerEvent.AddListener(HandTrigger);
+        }
     }
 
     // Update is called once per frame
@@ -62,13 +65,16 @@ public class PlayerController : MonoBehaviour
     /// Called when the hand touches another object
     /// </summary>
     /// <param name="other"></param>
-    private void HandTrigger(Collider other) {
-        if (other.CompareTag("Button")) {
+    private void HandTrigger(Collider other)
+    {
+        if (other.CompareTag("Button"))
+        {
             ButtonPressed();
         }
     }
 
-    private void ButtonPressed() {
+    private void ButtonPressed()
+    {
         if (buttonPressed)
             return;
 
@@ -82,19 +88,23 @@ public class PlayerController : MonoBehaviour
         hangingPig.GetComponent<Rigidbody>().isKinematic = true;
     }
 
-    private void ButtonUnPress() {
+    private void ButtonUnPress()
+    {
         buttonPressed = false;
     }
 
     /// <summary>
     /// Move player based on A and D button press
     /// </summary>
-    private void MovePlayer() {
-        if (Input.GetKey(KeyCode.A)) {
+    private void MovePlayer()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
             Vector3 targetPosition = Vector3.MoveTowards(transform.position, railA.transform.position, Time.deltaTime * moveSpeed);
             transform.position = targetPosition;
         }
-        if (Input.GetKey(KeyCode.D)) {
+        if (Input.GetKey(KeyCode.D))
+        {
             Vector3 targetPosition = Vector3.MoveTowards(transform.position, railB.transform.position, Time.deltaTime * moveSpeed);
             transform.position = targetPosition;
         }
@@ -103,11 +113,14 @@ public class PlayerController : MonoBehaviour
     /// <summary>
     /// Rotate player based on A and D button press
     /// </summary>
-    private void RotatePlayer() {
-        if (Input.GetKey(KeyCode.A)) {
+    private void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
             transform.Rotate(-Vector3.up * Time.deltaTime * rotateSpeed);
         }
-        if (Input.GetKey(KeyCode.D)) {
+        if (Input.GetKey(KeyCode.D))
+        {
             transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
         }
     }
@@ -115,12 +128,15 @@ public class PlayerController : MonoBehaviour
     /// <summary>
     /// Position and rotate the arm based on mouse position
     /// </summary>
-    private void PositionHand() {
+    private void PositionHand()
+    {
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         Hand.transform.position = transform.position + Vector3.up * handHeight + ray.direction * handDistance;
 
-        if (stretching && Input.GetMouseButtonUp(0)) {
-            if(heldPig != null) {
+        if (stretching && Input.GetMouseButtonUp(0))
+        {
+            if (heldPig != null)
+            {
                 heldPig.transform.parent = null;
                 heldPig.GetComponent<Rigidbody>().isKinematic = false;
                 heldPig = null;
@@ -130,9 +146,11 @@ public class PlayerController : MonoBehaviour
         stretching = Input.GetMouseButton(0);
 
 
-        if (stretching && handDistance < handDistanceStretched) {
+        if (stretching && handDistance < handDistanceStretched)
+        {
             handDistance += Time.deltaTime * stretchSpeed;
-            if(handDistance >= handDistanceStretched) {
+            if (handDistance >= handDistanceStretched)
+            {
                 GrabItem();
             }
         }
@@ -144,11 +162,14 @@ public class PlayerController : MonoBehaviour
     /// <summary>
     /// Grab pig near hand
     /// </summary>
-    private void GrabItem() {
+    private void GrabItem()
+    {
         Collider[] hitColliders = Physics.OverlapSphere(Hand.transform.position, handReach);
 
-        foreach(Collider c in hitColliders) {
-            if (c.CompareTag("Pig")) {
+        foreach (Collider c in hitColliders)
+        {
+            if (c.CompareTag("Pig"))
+            {
                 heldPig = c.gameObject;
                 heldPig.GetComponent<Rigidbody>().isKinematic = true;
                 heldPig.transform.parent = Hand.transform;
