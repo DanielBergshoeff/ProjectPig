00c1bab8908eaf560817e8d5a7d5b2508a772d3a
DanielBergshoeff <dabergshoeff@gmail.com>
2019-12-10 16:47:45 +0100

    Started on bots

---
+++ Assets/Scripts/Bystander/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get;  private set; }
+
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
