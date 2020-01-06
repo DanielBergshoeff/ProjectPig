d58f6ea3de941bbcc498b6c83ac3872807137e10
DanielBergshoeff <dabergshoeff@gmail.com>
2020-01-06 16:16:25 +0100

    Quick fixes

--- Assets/Scripts/Perp/DehairingPig.cs
+++ Assets/Scripts/Perp/DehairingPig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Pig : MonoBehaviour
+public class DehairingPig : MonoBehaviour
 {
     public GameObject FixedJointObject;
     public bool Dehaired = false;
