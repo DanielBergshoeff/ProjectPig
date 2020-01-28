94907854a2800c1f236130f61353dc8d3b56a7cb
Bastien <bas.dijkstra@student.hku.nl>
2020-01-28 16:41:24 +0100

    Auto stash before merge of "backupbranch2" and "origin/backupbranch3"

---
+++ Assets/Scripts/MenuSystem/MainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
